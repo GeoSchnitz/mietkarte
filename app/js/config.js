d004b512b8d75ac3a41cbc7a77f1c0386a214f06
GeoSchnitz <benjamin@schnitzer.com.de>
2016-05-30 15:12:14 +0200

    bbox

--- app/js/config.js
+++ app/js/config.js
@@ -1,6 +1,6 @@
 var config = {
   center: [50.12, 8.66],
-  bounds: L.latLngBounds([50.00, 8.48,], [50.23, 8.80]),
+  bounds: L.latLngBounds([49.98, 8.40,], [50.28, 8.89]),
   minSalary: 0,
   maxSalary: 8000,
   startSalary: 2500,
