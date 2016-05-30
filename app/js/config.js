88299d12918ae44250173a3ef5f4474c6dd4bb15
GeoSchnitz <benjamin@schnitzer.com.de>
2016-05-30 12:16:13 +0200

    descriptions

--- app/js/config.js
+++ app/js/config.js
@@ -2,8 +2,8 @@ var config = {
   center: [50.12, 8.66],
   bounds: L.latLngBounds([50.00, 8.48,], [50.23, 8.80]),
   minSalary: 0,
-  maxSalary: 6000,
-  startSalary: 3000,
+  maxSalary: 8000,
+  startSalary: 3400,
   startRoom: 2,
   colors: ['#d9ef8b', '#ffeaad', '#fdae61', '#d73027', '#a50026'],
   roomQms: {
