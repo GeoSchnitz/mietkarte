f6a8612c5d9c637bec3b2d5280f6844556a12516
GeoSchnitz <benjamin@schnitzer.com.de>
2016-06-01 07:12:51 +0200

    add chardinjs

--- app/js/config.js
+++ app/js/config.js
@@ -2,7 +2,7 @@ var config = {
   center: [50.12, 8.66],
   bounds: L.latLngBounds([49.98, 8.40,], [50.28, 8.89]),
   minSalary: 0,
-  maxSalary: 8000,
+  maxSalary: 7000,
   startSalary: 2500,
   startRoom: 2,
   colors: ['#d9ef8b', '#ffeaad', '#fdae61', '#d73027', '#a50026'],
